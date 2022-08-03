71d570c9edd3a90d9f5689e1da991a16191161f6
pranav <katkarpranav4@gmail.com>
2022-08-03 16:53:36 +0530

    defined all the components and created routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./components/Login";
+import NotFound from "./components/NotFound";
+import Home from "./components/Home";
+import RankingPage from "./components/RankingPage";
+import Results from "./components/Results";
 
 function App() {
   return (
-    <div className="App">
-      <Login />
-    </div>
+    <Router>
+      <div className="App">
+        {/* <Login /> */}
+        <Routes>
+          <Route exact path="/" element={<Login />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/rankingpage" element={<RankingPage />} />
+          <Route path="/result" element={<Results />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
