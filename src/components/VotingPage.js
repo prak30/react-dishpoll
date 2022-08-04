5e3114d7e369a379f197d0b647c4e8d27c374b5f
pranav <katkarpranav4@gmail.com>
2022-08-04 23:07:44 +0530

    done with voting page components which fetches api data and renders a card component

--- src/components/VotingPage.js
+++ src/components/VotingPage.js
@@ -1,7 +1,61 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import Card from "./Card";
 
 const VotingPage = () => {
-  return <div>VotingPage</div>;
+  const navigate = useNavigate();
+  const [data, setData] = useState([]);
+  const [first, setFirst] = useState(1);
+  const [second, setSecond] = useState(2);
+  const [third, setThird] = useState(3);
+
+  useEffect(() => {
+    fetch("https://raw.githubusercontent.com/syook/react-dishpoll/main/db.json")
+      .then((res) => res.json())
+      .then((data) => setData(data));
+  }, []);
+
+  const submitHandler = () => {
+    navigate("/result");
+  };
+
+  const handle1 = (e) => {
+    if (e !== second && e !== third) {
+      localStorage.setItem("rank1", e);
+      setFirst(e);
+    }
+  };
+  const handle2 = (e) => {
+    if (e !== first && e !== third) {
+      localStorage.setItem("rank2", e);
+      setSecond(e);
+    }
+  };
+  const handle3 = (e) => {
+    if (e !== second && e !== first) {
+      localStorage.setItem("rank3", e);
+      setThird(e);
+    }
+  };
+
+  return (
+    <div>
+      <h1>Choose top 3 favourites</h1>
+      {first !== null && second !== null && third !== null ? (
+        <button onClick={submitHandler}>SUBMIT</button>
+      ) : (
+        <Card
+          data={data}
+          first={first}
+          second={second}
+          third={third}
+          handle1={handle1}
+          handle2={handle2}
+          handle3={handle3}
+        />
+      )}
+    </div>
+  );
 };
 
 export default VotingPage;
